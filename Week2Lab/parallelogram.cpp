0010d466953a358b8463b8190583a1a3ba64439e
talha8765 <talhacanyon98@gmail.com>
2023-10-07 18:40:52 +0500

    Week2Lab

---
+++ Week2Lab/parallelogram.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+main(){
+cout<<"          ________________          "<<endl;
+cout<<"         /               /          "<<endl;
+cout<<"        /               /           "<<endl;
+cout<<"       /               /            "<<endl;
+cout<<"      /               /             "<<endl;
+cout<<"     /               /              "<<endl;
+cout<<"     ________________               "<<endl;
+
+}
---
+++ Week2Lab/square.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+main(){
+cout<<"          ________________          "<<endl;
+cout<<"         |                |         "<<endl;
+cout<<"         |                |         "<<endl;
+cout<<"         |                |         "<<endl;
+cout<<"         |                |         "<<endl;
+cout<<"         |                |         "<<endl;
+cout<<"         |                |         "<<endl;
+cout<<"          ________________          "<<endl;
+
+}
